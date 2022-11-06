0187bcbe20fb57ca2356fca04eefa13e629923cf
Ahbab Shahin Chowdhury <ahbabshahin7788@gmail.com>
2022-11-06 19:18:18 +0600

    UI been completed

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,6 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
+
+import 'dart:ffi';
 
 import 'package:afnus_diary/colors.dart';
 import 'package:afnus_diary/models/todo.dart';
@@ -49,7 +51,52 @@ class _AfnusDiaryState extends State<AfnusDiary> {
                     ),
                   )
                 ],
-              ))
+              )),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                    child: Container(
+                  margin: EdgeInsets.only(bottom: 20, left: 20, right: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0.0, 0.0),
+                        blurRadius: 10.0,
+                        spreadRadius: 0.0,
+                      )
+                    ],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Add a new todo item",
+                      border: InputBorder.none,
+                    ),
+                  ),
+                )),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20, right: 20),
+                  child: ElevatedButton(
+                    child: Text(
+                      "+",
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: tdBlue,
+                      // minimumSize: Size(60, 60),
+                      elevation: 10,
+                    ),
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
